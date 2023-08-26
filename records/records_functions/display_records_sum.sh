0958766db92c3f6d284778b1e021c0131346e517
Ella Gonin <79975111+ellaangel@users.noreply.github.com>
2023-08-26 23:47:15 +0300

    Update display_records_sum.sh

--- records/records_functions/display_records_sum.sh
+++ records/records_functions/display_records_sum.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# This function displays the total number of copies from all records
+############### Displays the total number of copies from all records #################
 display_records_sum() {
     local total_copies=0
     while IFS=',' read -r record_name num_of_copies; do
